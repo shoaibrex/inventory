6e3d635340571cfa2afd398050f37fd832b6692a
shoaib <nu.shoaib@gmail.com>
2022-02-01 14:34:19 +0500

    bill item issue fixed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -55,6 +55,7 @@ class OrdersController < ApplicationController
   def create
     if params[:item_id].present? && params[:item_id].count != 0 && params["order"]["member_id"] != nil
       @order = Order.create(member_id: params["order"]["member_id"])
+      params[:item_id].shift
       params[:item_id].count.times do |i|
         if params[:quantity][i].to_i != 0 && params[:price][i].to_i != 0
           OrdersItem.create(order_id: @order.id, item_id: params[:item_id][i].to_i, quantity: params[:quantity][i].to_i, unit_price: params[:price][i].to_i, total_price: params[:price][i].to_i*params[:quantity][i].to_i)
