64709f26283da83491221cff9e230f289d495924
shoaib <nu.shoaib@gmail.com>
2022-12-15 15:03:07 +0500

    deploy puma settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,20 +17,20 @@ append :rbenv_map_bins, 'puma', 'pumactl'
 # You can configure the Airbrussh format using :format_options.
 # These are the defaults.
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+# set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+# set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+# set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_bind, "unix://home/ubuntu/inventory/shared/tmp/sockets/puma.sock"    #accept array for multi-bind
-set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_error.log"
-set :puma_error_log, "#{shared_path}/log/puma_access.log"
-set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+# set :puma_conf, "#{shared_path}/puma.rb"
+# set :puma_access_log, "#{shared_path}/log/puma_error.log"
+# set :puma_error_log, "#{shared_path}/log/puma_access.log"
+# set :puma_role, :app
+# set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
 set :puma_threads, [0, 8]
-set :puma_workers, 0
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true
-set :puma_preload_app, false
+set :puma_workers, 2
+# set :puma_worker_timeout, nil
+# set :puma_init_active_record, true
+# set :puma_preload_app, false
 # Default value for :pty is false
 # set :pty, true
 set :pty, true
