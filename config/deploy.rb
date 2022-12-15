f1452e0e0670fec51ae6290b63936764631f23aa
shoaib <nu.shoaib@gmail.com>
2022-12-15 15:30:40 +0500

    deploy puma settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ append :rbenv_map_bins, 'puma', 'pumactl'
 # set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 # set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 # set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://home/ubuntu/inventory/shared/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_bind, "unix:///home/ubuntu/inventory/shared/tmp/sockets/puma.sock"    #accept array for multi-bind
 # set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_access.log"
 set :puma_error_log, "#{shared_path}/log/puma_error.log"
